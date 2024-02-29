dc4a2a8cc0773cc33502d4c81711858627987635
GazinurDavletbaev <gyxtochka@mail.ru>
2024-02-29 12:31:05 +0400

    добавлен обработчик merge

--- telegabot/telegabot.py
+++ telegabot/telegabot.py
@@ -7,6 +7,7 @@ bot = telebot.TeleBot('7084082443:AAGrqnOeAa7ub1vW1LRytZdN0pzLIZhHYT4')
 def main(message):
     bot.send_message(message.chat.id, 'Privet Danja')
 
+
 @bot.message_handler(commands=['info'])
 def main(message):
     bot.send_message(message.chat.id, 'Privet Danja')
@@ -15,5 +16,16 @@ def main(message):
 @bot.message_handler(commands=['stop'])
 def main(message):
     bot.send_message(message.chat.id, 'Privet Danja')
+
+
+@bot.message_handler(commands=['branch dev'])
+def main(message):
+    bot.send_message(message.chat.id, 'Privet Danja')
+
+
+@bot.message_handler(commands=['merge master'])
+def main(message):
+    bot.send_message(message.chat.id, 'Privet Danja')
+
 
 bot.polling(none_stop=True)
