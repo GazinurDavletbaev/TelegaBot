5cce244e8f9a401e7807d8cd7cfa4ca7dd1dd09c
GazinurDavletbaev <gyxtochka@mail.ru>
2024-02-29 12:17:32 +0400

    init

---
+++ telegabot/telegabot.py
@@ -0,0 +1,15 @@
+import telebot
+
+bot = telebot.TeleBot('7084082443:AAGrqnOeAa7ub1vW1LRytZdN0pzLIZhHYT4')
+
+
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, 'Privet Danja')
+
+@bot.message_handler(commands=['info'])
+def main(message):
+    bot.send_message(message.chat.id, 'Privet Danja')
+
+
+bot.polling(none_stop=True)
